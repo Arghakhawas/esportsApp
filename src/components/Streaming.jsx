277f8b5507b0dedd08a9e5be8821b19d9839ea27
Arghakhawas <argha820gmail.com>
2024-02-22 00:18:49 +0530

    update

--- src/components/Streaming.jsx
+++ src/components/Streaming.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef,useEffect } from 'react';
 import Webcam from 'react-webcam';
 import { ScreenCapture } from "react-screen-capture";
 
