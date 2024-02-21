4dbb5581164de7ef9e1d1fe6f37b76cead920264
Arghakhawas <argha820gmail.com>
2024-02-21 15:45:21 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-awesome-loaders'], // Add the library name here
-    },
-  },
 });
