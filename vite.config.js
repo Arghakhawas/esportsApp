947a1989751e86ec8cd5f02345c9e865d1dc8799
Arghakhawas <argha820gmail.com>
2024-02-21 15:42:47 +0530

    react-awsome-loaders

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import reactPlugin from '@vitejs/plugin-react';
 
-export default {
-  plugins: [reactPlugin()],
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react-awesome-loaders'], // Add the library name here
     },
   },
-};
+});
