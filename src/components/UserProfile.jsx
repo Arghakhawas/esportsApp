833b0d6e20fb905be9ba0bfa1584dd615d5cd453
Arghakhawas <argha820@gmail.com>
2024-02-22 16:33:32 +0530

    size maintain

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,6 +6,7 @@ import ChangePasswordModal from './ChangePasswordModel';
 const UserProfile = () => {
   const [profileData, setProfileData] = useState(null);
   const [showChangePasswordModal, setShowChangePasswordModal] = useState(false);
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -29,6 +30,9 @@ const UserProfile = () => {
 
     fetchUserProfile();
   }, []);
+  const handleAvatarSelection = (avatar) => {
+    setSelectedAvatar(avatar);
+  };
 
   const handleOpenChangePasswordModal = () => {
     setShowChangePasswordModal(true);
@@ -43,9 +47,32 @@ const UserProfile = () => {
       <h2 className="profile-heading">User Profile</h2>
       {profileData && (
         <div className="profile-details">
-          {/* Display User Profile Picture */}
-          <img src={profileData.profilePicture} alt="Profile" className="profile-picture" />
+       
+       <img
+            src={selectedAvatar || `https://ui-avatars.com/api/?name=${generateProfileAvatar(profileData.username)}&background=random`}
+            alt="Profile"
+            className="profile-picture"
+          />
 
+          {/* Avatar selection section */}
+          <div className="avatar-selection">
+            <h3>Choose Avatar</h3>
+            <div className="avatar-options">
+              <img
+                src="avatar_url_1.jpg"
+                alt="Avatar 1"
+                onClick={() => handleAvatarSelection("avatar_url_1.jpg")}
+              />
+              <img
+                src="avatar_url_2.jpg"
+                alt="Avatar 2"
+                onClick={() => handleAvatarSelection("avatar_url_2.jpg")}
+              />
+              {/* Add more avatars as needed */}
+            </div>
+          </div>
+
+          
           <p className="profile-info">Name: {profileData.username}</p>
           <p className="profile-info">Email: {profileData.email}</p>
           <p className="profile-info">Refer id: {profileData.referId}</p>
