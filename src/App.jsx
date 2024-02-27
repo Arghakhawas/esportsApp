25f2c33e98b5f56548a000647bb5356e53866660
Arghakhawas <argha820@gmail.com>
2024-02-28 03:18:42 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// Import necessary modules and components
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -21,12 +20,11 @@ import AdminPanel from './components/Admin/AdminPanel';
 import AdminLogin from './components/Admin/Adminlogin';
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(false); // Add isAdmin state
+  const [isAdmin, setIsAdmin] = useState(false);
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleLoginSuccess = (userData) => {
-    console.log(userData); // Check the structure of userData
     setUser(userData.user);
     setIsAuthenticated(true);
     setIsAdmin(userData.user?.isAdmin || false);
@@ -35,7 +33,7 @@ function App() {
   const handleLogout = () => {
     setUser(null);
     setIsAuthenticated(false);
-    setIsAdmin(false); // Reset isAdmin on logout
+    setIsAdmin(false);
   };
 
   const handleSignup = (userData) => {
@@ -80,20 +78,15 @@ function App() {
           )}
 
           {/* Admin Routes */}
-          <Route
-            path="/admin/login"
-            element={<AdminLogin onLoginSuccess={handleLoginSuccess} />}
-          />
-          <Route
-            path="/admin"
-            element={
-              isAdmin ? (
-                <AdminPanel />
-              ) : (
-                <Navigate to="/admin/login" />
-              )
-            }
-          />
+          {isAuthenticated && isAdmin && (
+            <>
+              <Route path="/admin/login" element={<AdminLogin onLoginSuccess={handleLoginSuccess} />} />
+              <Route path="/admin" element={<AdminPanel />} />
+            </>
+          )}
+          {isAuthenticated && !isAdmin && (
+            <Route path="/admin" element={<Navigate to="/admin/login" />} />
+          )}
         </Routes>
       </div>
     </BrowserRouter>
