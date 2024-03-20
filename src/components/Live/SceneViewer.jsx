6cf5e48bfbc10aadf5a908a9a9e9d2fc062cb317
Arghakhawas <argha820@gmail.com>
2024-03-20 17:25:13 +0530

    live

--- src/components/Live/SceneViewer.jsx
+++ src/components/Live/SceneViewer.jsx
@@ -2,39 +2,40 @@ import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import LiveSceneViewer from "./LiveSceneViewer";
 
-const SceneViewer = () => {
-  const [remoteStreams, setRemoteStreams] = useState([]);
-  const socket = useRef(null);
-
-  useEffect(() => {
-    // Connect to the Socket.io server
-    socket.current = io("https://esportsappbackend.onrender.com");
-
-    // Listen for stream events
-    socket.current.on("stream", (remoteStream) => {
-      setRemoteStreams((prevStreams) => [...prevStreams, remoteStream]);
-    });
-
-    // Listen for stopStream event
-    socket.current.on("stopStream", () => {
-      setRemoteStreams([]);
-    });
-
-    return () => {
-      socket.current.disconnect();
-    };
-  }, []);
-
-  return (
-    <div>
-      <h2>Scene Viewer</h2>
-      {remoteStreams.length > 0 ? (
-        <LiveSceneViewer remoteStreams={remoteStreams} />
-      ) : (
-        <p>No live streams available</p>
-      )}
-    </div>
-  );
-};
+  const SceneViewer = () => {
+    const [remoteStreams, setRemoteStreams] = useState([]);
+  
+    useEffect(() => {
+      // Connect to the Socket.io server
+      const socket = io("https://esportsappbackend.onrender.com");
+  
+      // Listen for stream events
+      socket.on("mobile-stream", (stream) => {
+        setRemoteStreams((prevStreams) => [...prevStreams, stream]);
+      });
+  
+      // Listen for stopStream event
+      socket.on("stopStream", () => {
+        setRemoteStreams([]);
+      });
+  
+      return () => {
+        socket.disconnect();
+      };
+    }, []);
+  
+    return (
+      <div>
+        <h2>Scene Viewer</h2>
+        {remoteStreams.length > 0 ? (
+          remoteStreams.map((stream, index) => (
+            <video key={index} srcObject={stream} autoPlay playsInline />
+          ))
+        ) : (
+          <p>No live streams available</p>
+        )}
+      </div>
+    );
+  };
 
 export default SceneViewer;
